90289bdcd761014af823e7cf929f8a16d9a8f9cc
Joseph H Kennedy <me@jhkennedy.org>
2020-12-28 17:26:29 -0900

    A little simplification

--- bullets/__main__.py
+++ bullets/__main__.py
@@ -1,110 +1,103 @@
+import argparse
 from datetime import datetime
+from typing import Optional
 
 from dateutil.parser import parse as parse_date
 from dateutil.relativedelta import relativedelta
 from dateutil import tz
 from ghapi.core import GhApi
 
-from bullets import create
+from bullets import util
 
-aknow = datetime.now(tz.gettz('AKST'))
-# TODO: as CLI option
-search_start = aknow - relativedelta(days=21, hour=14, minute=0, second=0, microsecond=0)
+_DAYS_BACK = 7
+_RD_ARGS = {'hour': 14, 'minute': 0, 'second': 0, 'microsecond': 0}
 
-gh = GhApi()
 
-meta = {
-    'title': 'Tools Team bullets',
-    'description': f"Tools team bullets for {search_start.isoformat(timespec='seconds')}"
-                   f" through {aknow.isoformat(timespec='seconds')}",
-}
+def generate_bullets(search_start: Optional[datetime] = None, detailed: bool = False):
+    aknow = datetime.now(tz.gettz('AKST'))
+    if search_start is None:
+        search_start = aknow - relativedelta(days=_DAYS_BACK, **_RD_ARGS)
 
-release_details = {}
-dev_prs = {}
-open_prs = {}
-opened_issues = {}
-for repo in gh.repos.list_for_org('ASFHyP3'):
-    last_release = None
-    for release in gh.repos.list_releases(repo.owner.login, repo.name):
-        created_at = parse_date(release.created_at)
+    meta = {
+        'title': 'Tools Team bullets',
+        'description': f"Tools team bullets for {search_start.isoformat(timespec='seconds')}"
+                       f" through {aknow.isoformat(timespec='seconds')}",
+    }
+
+    gh = GhApi()
+    release_details = {}
+    dev_prs = {}
+    open_prs = {}
+    opened_issues = {}
+    for repo in gh.repos.list_for_org('ASFHyP3'):
+        last_release = None
+        for release in gh.repos.list_releases(repo.owner.login, repo.name):
+            created_at = parse_date(release.created_at)
+            if last_release is None:
+                # FIXME: Hacky way to get last release time
+                last_release = created_at
+            if created_at >= search_start:
+                release_details[release.target_commitish] = util.get_details(release)
+            else:
+                break
+
         if last_release is None:
-            # FIXME: Hacky way to get last release time
-            last_release = created_at
-        if created_at >= search_start:
-            release_details[release.target_commitish] = {
-                'name': release.name,
-                'title': '',
-                'body': release.body,
-                'html_url': release.html_url,
-                'created_at': created_at,
-            }
-        else:
-            break
+            last_release = search_start
+        for pr in gh.pulls.list(repo.owner.login, repo.name,
+                                state='closed', base='develop', sort='updated', direction='desc'):
+            last_updated = parse_date(pr.updated_at)
+            # FIXME: This probably should check if merged, and if the merge commit is *in* head.
+            #        This assumes that everything merged into develop before the release was included
+            #        in the release
+            if last_updated >= last_release and pr.get('merged', False):
+                dev_prs[pr.merge_commit_sha] = util.get_details(pr)
 
-    if last_release is None:
-        last_release = search_start
-    for pr in gh.pulls.list(repo.owner.login, repo.name,
-                            state='closed', base='develop', sort='updated', direction='desc'):
-        last_updated = parse_date(pr.updated_at)
-        # FIXME: This probably should check if merged, and if the merge commit is *in* head.
-        #        This assumes that everything merged into develop before the release was included
-        #        in the release
-        if last_updated >= last_release and pr.get('merged', False):
-            dev_prs[pr.merge_commit_sha] = {
-                'name': f'{repo.owner.login}/{repo.name} #{pr.number}',
-                'number': pr.number,
-                'title': pr.title,
-                'body': pr.body,
-                'html_url': pr.html_url,
-                'last_updated': last_updated,
-            }
+        for pr in gh.pulls.list(repo.owner.login, repo.name,
+                                state='open', sort='created', direction='desc'):
+            open_prs[pr.head.sha] = util.get_details(pr)
 
-    for pr in gh.pulls.list(repo.owner.login, repo.name,
-                            state='open', sort='created', direction='desc'):
-        created_at = parse_date(pr.created_at)
-        open_prs[pr.head.sha] = {
-            'name': f'{repo.owner.login}/{repo.name} #{pr.number}',
-            'title': pr.title,
-            'body': pr.body,
-            'html_url': pr.html_url,
-            'created_at': created_at,
-        }
+        # FIXME: Returns issues and PRs... simpler to filter this one list or make the three calls?
+        for issue in gh.issues.list_for_repo(repo.owner.login, repo.name,
+                                             state='open', sort='created', direction='desc',
+                                             since=search_start.isoformat(timespec='seconds')):
+            if issue.get('pull_request') is None:
+                opened_issues[issue.id] = util.get_details(issue)
 
-    # FIXME: Returns issues and PRs... simpler to filter this one list or make the three calls?
-    for issue in gh.issues.list_for_repo(repo.owner.login, repo.name,
-                                         state='open', sort='created', direction='desc',
-                                         since=search_start.isoformat(timespec='seconds')):
-        if issue.get('pull_request') is None:
-            created_at = parse_date(issue.created_at)
-            opened_issues[issue.id] = {
-                'name': f'{repo.owner.login}/{repo.name} #{issue.number}',
-                'title': issue.title,
-                'body': issue.body,
-                'html_url': issue.html_url,
-                'created_at': created_at,
-            }
+    template = 'report_detailed.md.j2' if detailed else 'report.md.j2'
+    report_name = 'report_detailed.md' if detailed else 'report.md'
+    report = util.render_template(
+        template,
+        releases=release_details,
+        meta=meta,
+        dev_prs=dev_prs,
+        open_prs=open_prs,
+        opened_issues=opened_issues,
+    )
+    with open(report_name, 'w') as f:
+        f.write(report)
 
-# TODO: report format CLI option (email, md, ...)
-# TODO: out file name CLI option
-# TODO: detailed CLI option
-report = create.render_template(
-    'report.md.j2',
-    releases=release_details,
-    meta=meta,
-    dev_prs=dev_prs,
-    open_prs=open_prs,
-    opened_issues=opened_issues,
-)
-with open('report.md', 'w') as f:
-    f.write(report)
 
-report_detailed = create.render_template(
-    'report_detailed.md.j2',
-    releases=release_details,
-    meta=meta,
-    dev_prs=dev_prs,
-    open_prs=open_prs,
-    opened_issues=opened_issues,
-)
-with open('report_detailed.md', 'w') as f:
-    f.write(report_detailed)
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    begin = parser.add_mutually_exclusive_group()
+    begin.add_argument('-d', '--days-back', type=int, default=_DAYS_BACK,
+                       help='Start search at 2:00 PM this many days ago')
+    begin.add_argument('-s', '--start-search', type=parse_date,
+                       help='Start search at this time (must be parsable by dateutil)')
+
+    parser.add_argument('--detailed', action='store_true',
+                        help='Produce a detailed report that includes the Release/PR/Issue body')
+
+    args = parser.parse_args()
+
+    if args.start_search is None:
+        args.start_search = datetime.now(tz.gettz('AKST')) - relativedelta(days=args.days_back, **_RD_ARGS)
+
+    generate_bullets(search_start=args.start_search, detailed=args.detailed)
+
+
+if __name__ == '__main__':
+    main()
--- bullets/util.py
+++ bullets/util.py
@@ -1,4 +1,6 @@
 import jinja2
+from dateutil.parser import parse as parse_date
+from fastcore.basics import AttrDict
 
 
 def get_environment() -> jinja2.Environment:
@@ -18,3 +20,20 @@ def render_template(template: str, **kwargs) -> str:
     template = env.get_template(template)
     rendered = template.render(**kwargs)
     return rendered
+
+
+def get_details(node: AttrDict) -> dict:
+    name = node.get('name')
+    if name is None:
+        repo = '/'.join(node.html_url.split('/')[-4:-2])
+        name = f'{repo} #{node.number}'
+
+    created_at = parse_date(node.created_at)
+    return {
+        'name': name,
+        'title': node.get('title', ''),
+        'body': node.body,
+        'html_url': node.html_url,
+        'created_at': created_at,
+    }
+

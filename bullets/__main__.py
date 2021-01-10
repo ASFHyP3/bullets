8204f532a0ba2d71ff3877a64f522dfc866f167c
Joseph H Kennedy <me@jhkennedy.org>
2021-01-09 20:45:57 -0900

    simplify latest release and bugfix test prs

--- bullets/__main__.py
+++ bullets/__main__.py
@@ -5,7 +5,9 @@ from typing import Optional
 from dateutil.parser import parse as parse_date
 from dateutil.relativedelta import relativedelta
 from dateutil import tz
+from fastcore.net import HTTP404NotFoundError
 from ghapi.core import GhApi
+from tqdm import tqdm
 
 from bullets import util
 
@@ -29,40 +31,34 @@ def generate_bullets(search_start: Optional[datetime] = None, detailed: bool = F
     dev_prs = {}
     open_prs = {}
     opened_issues = {}
-    for repo in gh.repos.list_for_org('ASFHyP3'):
-        last_release = None
-        for release in gh.repos.list_releases(repo.owner.login, repo.name):
-            created_at = parse_date(release.created_at)
-            if last_release is None:
-                # FIXME: Hacky way to get last release time
-                last_release = created_at
-            if created_at >= search_start:
-                release_details[release.target_commitish] = util.get_details(release)
-            else:
-                break
-
-        if last_release is None:
-            last_release = search_start
-        # FIXME: might be able to use issues.list_for_repo with since=... to simplify logic
-        for pull in gh.pulls.list(repo.owner.login, repo.name,
-                                  state='closed', base='develop', sort='updated', direction='desc'):
-            last_updated = parse_date(pull.updated_at)
-            # FIXME: This probably should check if merged, and if the merge commit is *in* head.
-            #        This assumes that everything merged into develop before the release was included
-            #        in the release
-            if last_updated >= last_release and pull.get('merged', False):
-                dev_prs[pull.merge_commit_sha] = util.get_details(pull)
-
-        for pull in gh.pulls.list(repo.owner.login, repo.name,
-                                  state='open', sort='created', direction='desc'):
-            open_prs[pull.head.sha] = util.get_details(pull)
-
+    for repo in tqdm(gh.repos.list_for_org('ASFHyP3')):
         # FIXME: Returns issues and PRs... simpler to filter this one list or make the three calls?
         for issue in gh.issues.list_for_repo(repo.owner.login, repo.name,
                                              state='open', sort='created', direction='desc',
                                              since=search_start.isoformat(timespec='seconds')):
             if issue.get('pull_request') is None:
                 opened_issues[issue.id] = util.get_details(issue)
+
+        try:
+            last_release = parse_date(gh.repos.get_latest_release(repo.owner.login, repo.name).created_at)
+            for release in gh.repos.list_releases(repo.owner.login, repo.name):
+                created_at = parse_date(release.created_at)
+                if created_at >= search_start:
+                    release_details[release.target_commitish] = util.get_details(release)
+                else:
+                    break
+        except HTTP404NotFoundError:
+            last_release = search_start
+
+        # FIXME: might be able to use issues.list_for_repo with since=... to simplify logic
+        for pull in gh.pulls.list(repo.owner.login, repo.name,
+                                  state='closed', base='develop', sort='updated', direction='desc'):
+            if (merged_at := pull.get('merged_at')) and parse_date(merged_at) > max(search_start, last_release):
+                dev_prs[pull.merge_commit_sha] = util.get_details(pull)
+
+        for pull in gh.pulls.list(repo.owner.login, repo.name,
+                                  state='open', sort='created', direction='desc'):
+            open_prs[pull.head.sha] = util.get_details(pull)
 
     template = 'report_detailed.md.j2' if detailed else 'report.md.j2'
     report_name = 'report_detailed.md' if detailed else 'report.md'

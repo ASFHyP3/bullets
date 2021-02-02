2744c1ff56fe30572602db83fecccab43cc8cd09
Joseph H Kennedy <me@jhkennedy.org>
2021-02-01 15:05:56 -0900

    Ensure TZ aware dates on the CLI
    fixes #3

--- bullets/__main__.py
+++ bullets/__main__.py
@@ -18,7 +18,8 @@ def main():
     begin.add_argument('-d', '--days-back', type=int, default=_DAYS_BACK,
                        help='Start search at 2:00 PM this many days ago')
     begin.add_argument('-s', '--start-search', type=parse_date,
-                       help='Start search at this time (must be parsable by dateutil)')
+                       help='Start search at this time. Time must be parsable by dateutil '
+                            'and AKST will be assumed for the time zone if not given.')
 
     parser.add_argument('--detailed', action='store_true',
                         help='Produce a detailed report that includes the Release/PR/Issue body. '
@@ -28,6 +29,9 @@ def main():
 
     if args.start_search is None:
         args.start_search = datetime.now(tz.gettz('AKST')) - relativedelta(days=args.days_back, **_RD_ARGS)
+
+    if args.start_search.tzinfo is None:
+        args.start_search = args.start_search.replace(tzinfo=tz.gettz('AKST'))
 
     generate_bullets(search_start=args.start_search, detailed=args.detailed)
 
